498b53976051a57b8d8232e06dba124b6907586f
Pravesh-Jamgade <praveshjamgade@gmail.com>
2022-10-23 18:34:49 +0530

    add

--- auto.py
+++ auto.py
@@ -106,7 +106,7 @@ for fol in inputs:
             subprocess.run(['./config.sh'.format(curdir), 'champsim_config.json'])
             
             trace_inital = fol.split('.')[1]
-            cmd = "./bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 --trace_name {0} --policy {1} --size {2}".format(fol, replace_policy, size)
+            cmd = "./bin/champsim --warmup_instructions 50000000 --simulation_instructions 200000000 --trace_name {0} --policy {1} --size {2}".format(fol, replace_policy, size)
 
             with subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
                 op, er = proc.communicate()
