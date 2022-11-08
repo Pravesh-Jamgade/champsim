4f5a3cd82894b06add01370cc59ffce18a5b00a4
Pravesh-Jamgade <praveshjamgade@gmail.com>
2022-11-08 15:32:32 +0530

    added auto.py

--- auto.py
+++ auto.py
@@ -101,45 +101,42 @@ for fol in inputs:
             update(cache[0], "replacement", replace_policy)
 
             combi_str = "{}-{}.log".format(size, replace_policy)
-
+            result_str = "{},{} ".format(combi_str, fol)
+            
             #saving new setting
             json_string = json.dumps(cj)
             with open(config_file_path, 'w') as outfile:
                 outfile.write(json_string)
 
-            subprocess.run(['./config.sh'.format(curdir), 'champsim_config.json'])
-            subprocess.run(['make'])
+            try:
+                subprocess.run(['./config.sh'.format(curdir), 'champsim_config.json'])
+                result_status.append(result_str+"config=pass")
+            except:
+                result_status.append(result_str+"..config=fail")
+
+            try:
+                subprocess.run(['make'])
+                result_status.append(result_str+"..make=pass")
+            except:
+                result_status.append(result_str+"..make=fail")
             
             trace_path = os.path.join(curdir, "traces/{}".format(fol))
             trace_inital = fol.split('.')[1]
             cmd = "./bin/champsim --warmup_instructions 50000000 --simulation_instructions 200000000 {} --trace_name {} --policy {} --size {}".format(trace_path, fol, replace_policy, size)
-
-            result_str = "{},{} ".format(combi_str, fol)
-            proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            
             try:
+                proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 op, er = proc.communicate()
-                result_status.append(result_str+"..pass")
+                result_status.append(result_str+"..run=pass")
             except:
-                result_status.append(result_str+"..fail")
-            
-         
-            ## output cache.log and ipc.log
-            # for path in os.listdir(curdir):
-            #     file = os.path.join(curdir, path)
-            #     if os.path.isfile(file):
-            #         if path.endswith('.log'):
-            #             log_file_name = path.split('.')[0]#cache
-            #             if log_file_name == 'writes':
-            #                 continue
-            #             log_file_name =  log_file_name +'-'+ combi_str#cache-combi-str.log
-            #             newfile = os.path.join(savedir,log_file_name)
-            #             os.system('mv {} {}'.format(file, newfile))
-            
+                result_status.append(result_str+"..run=fail")
             
             result_status.append(result_str)
 
 default_json_file.close()
 
+frun = open("run", 'w')
 for st in result_status:
-    print(st, "..ok\n")
+    frun.write(str)
+frun.close()
             
