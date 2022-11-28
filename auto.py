a28e620c4333288a5322f7084d19bea0477d7c62
Pravesh-Jamgade <praveshjamgade@gmail.com>
2022-11-28 21:35:14 +0530

    p

--- auto.py
+++ auto.py
@@ -8,15 +8,15 @@ from os.path import exists
 import shutil
 
 inputs = [
-    '410.bwaves-945B.champsimtrace.xz',
-    '444.namd-120B.champsimtrace.xz',
+    # '410.bwaves-945B.champsimtrace.xz',
+    # '444.namd-120B.champsimtrace.xz',
     '445.gobmk-17B.champsimtrace.xz',
-    '447.dealII-3B.champsimtrace.xz',
-    '433.milc-127B.champsimtrace.xz',
-    '434.zeusmp-10B.champsimtrace.xz',
+    # '447.dealII-3B.champsimtrace.xz',
+    # '433.milc-127B.champsimtrace.xz',
+    # '434.zeusmp-10B.champsimtrace.xz',
     '435.gromacs-111B.champsimtrace.xz',
-    '436.cactusADM-1804B.champsimtrace.xz',
-    '437.leslie3d-134B.champsimtrace.xz',
+    # '436.cactusADM-1804B.champsimtrace.xz',
+    # '437.leslie3d-134B.champsimtrace.xz',
     '429.mcf-217B.champsimtrace.xz'
 ]
 
@@ -42,7 +42,11 @@ def combi(depth, j, bag):
 BLOCK_SIZE = 64
 
 ways = [8] #llc
-all_size = [0.5, 1, 2]#llc cache size in MB
+all_size = [
+    0.5,
+    # 1,
+    # 2
+]#llc cache size in MB
 cache = ['LLC']
 
 replacement = [
