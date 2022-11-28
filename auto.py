afab2d3a8ec7e3141f26c6d980a1694d8c106176
Pravesh-Jamgade <praveshjamgade@gmail.com>
2022-11-28 18:23:23 +0530

    added

--- auto.py
+++ auto.py
@@ -67,6 +67,10 @@ update("num_cores", "", 2)
 cj['ooo_cpu'].append(cj['ooo_cpu'][0])
 for fol in inputs:
 
+    folName = ""
+    for wl in fol:
+        folName = folName + "-" + wl.split('.')[1]
+    
     #foreach trace use reaplce policy
     for replace_policy in replacement:
 
@@ -91,12 +95,10 @@ for fol in inputs:
             with open(config_file_path, 'w') as outfile:
                 outfile.write(json_string)
 
-            trace_path1 = "traces/{}".format(fol[0].split('.')[1])
-            trace_path2 = "traces/{}".format(fol[1].split('.')[1])
-            trace_path3 = "traces/{}".format(fol[2].split('.')[1])
-            trace_path4 = "traces/{}".format(fol[3].split('.')[1])
-
-            folName = trace_path1+"-"+trace_path2+"-"+trace_path3+"-"+trace_path4
+            trace_path1 = "traces/{}".format(fol[0])
+            trace_path2 = "traces/{}".format(fol[1])
+            trace_path3 = "traces/{}".format(fol[2])
+            trace_path4 = "traces/{}".format(fol[3])
 
             combi_str = "{},{},{}".format(size, replace_policy, folName)
             
