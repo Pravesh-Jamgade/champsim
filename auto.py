89029e3cbf4c61f9536ee0b6d7217504ece8870b
Pravesh-Jamgade <praveshjamgade@gmail.com>
2022-11-29 22:40:45 +0530

    added failsafe

--- auto.py
+++ auto.py
@@ -111,7 +111,8 @@ for fol in inputs:
                 try:
                     subprocess.run(shlex.split(cmd))
                 except:
-                    frun.write("{} ..fail\n".format(combi_str))
+                    frun.write("{} for {}..fail\n".format(combi_str, cmd))
+                    print("{} for {}..fail\n".format(combi_str, cmd))
                     break
             frun.write("{} ..pass\n".format(combi_str))
             
