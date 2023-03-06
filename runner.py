a2ce8995ff827328f9fc13038c2a7bf41da7ea12
Pravesh-Jamgade <praveshjamgade@gmail.com>
2023-03-06 16:42:20 +0530

    update runner for new workloads

--- runner.py
+++ runner.py
@@ -6,20 +6,44 @@ import shlex
 from os.path import exists
 
 traces = [
-    '429.mcf-217B.champsimtrace.xz',
+    '429.mcf-51B.champsimtrace.xz',
     '437.leslie3d-134B.champsimtrace.xz',
     '433.milc-127B.champsimtrace.xz',
     '470.lbm-1274B.champsimtrace.xz',
-    '482.sphinx3-1100B.champsimtrace.xz'
+    'pr-10.trace.gz',
+    'bc-0.trace.gz',
+    'bfs-10.trace.gz',
+    'cc-13.trace.gz'
 ]
+
+mixes = [
+    [traces[0], traces[1], traces[4], traces[5]],
+    [traces[0], traces[2], traces[6], traces[7]],
+    [traces[0], traces[1], traces[2], traces[3]],
+    [traces[4], traces[5], traces[6], traces[7]],
+]
+
 def task1():
-    for trace in traces:
-        trace_list = f"traces/{trace} traces/{trace} traces/{trace} traces/{trace}"
-        cmd = f"./bin/champsim --warmup_instructions 10000000 --simulation_instructions 20000000 {trace_list} --trace_name {trace} --policy srrip --size 8"
+    # trace_list = f"traces/{trace} traces/{trace} traces/{trace} traces/{trace}"
+    trace_list = ""
+
+    for mix in mixes:
+        tag = ""
+        path = ""
+        for trace in mix:
+            path = path + f"traces/{trace} "
+            trace_list =  trace_list + path
+            if tag == "":
+                tag = trace.split('.')[0]
+            else:
+                tag = tag + "-" + trace.split('.')[0]
+        
+        print(f"{tag}\n{path}")
+        cmd = f"./bin/champsim --warmup_instructions 100000000 --simulation_instructions 200000000 {trace_list} --trace_name {tag} --policy lru --size 4"
         try:
             print(f"running... {cmd}")
             subprocess.run(shlex.split(cmd))
         except subprocess.CalledProcessError as e:
             print("error: ", e.output, trace)
-        break
+
 task1()
