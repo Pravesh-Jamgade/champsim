5bf989fdc3e3c40c0a0c8f97291f0baf43b383a8
Pravesh-Jamgade <praveshjamgade@gmail.com>
2023-03-17 17:38:17 +0530

    ad

---
+++ inc/V1Predictor.h
@@ -0,0 +1,20 @@
+#ifndef V1_PRED_H
+#define V1_PRED_H
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+using namespace std;
+
+
+class V1Predictor
+{
+    private:
+
+    map<>
+    public:
+
+};
+
+#endif
