7aa3a536a1e32e52554f48b2f46fcc9b1daa17ed
Pravesh-Jamgade <praveshjamgade@gmail.com>
2023-10-30 17:56:35 +0530

    test nextline

--- prefetcher/next_line/next_line.cc
+++ prefetcher/next_line/next_line.cc
@@ -7,7 +7,8 @@ void CACHE::prefetcher_initialize() { std::cout << NAME << " next line prefetche
 uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in)
 {
   uint64_t pf_addr = addr + (1 << LOG2_BLOCK_SIZE);
-  prefetch_line(ip, addr, pf_addr, true, 0);
+  if(metadata_in == 2)
+    prefetch_line(ip, addr, pf_addr, true, 0);
   return metadata_in;
 }
 
