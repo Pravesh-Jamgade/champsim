ec86ea2e7d28794582d010af84fb446b684373a8
Pravesh Jamgade <praveshjamgade@gmail.com>
2025-08-20 15:30:45 +0530

    fix trace

--- inc/trace_instruction.h
+++ inc/trace_instruction.h
@@ -23,6 +23,7 @@ struct context_instr {
 
   unsigned long long destination_memory[NUM_INSTR_DESTINATIONS] = {}; // output memory
   unsigned long long source_memory[NUM_INSTR_SOURCES] = {};           // input memory
+  char context = '0';
 };
 
 struct input_instr {
