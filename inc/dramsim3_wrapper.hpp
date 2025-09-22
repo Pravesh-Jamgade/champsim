5bb7dfce903360896aa9d3a1a454e1454f50b30a
Pravesh Jamgade <praveshjamgade@gmail.com>
2025-09-22 14:12:48 +0530

    Needs testing

--- inc/dramsim3_wrapper.hpp
+++ inc/dramsim3_wrapper.hpp
@@ -40,7 +40,7 @@ public:
             numPPages = (DRAM_CHANNELS * DRAM_RANKS * DRAM_BANKS 
                                 * DRAM_ROWS * DRAM_COLUMNS * BLOCK_SIZE) / PAGE_SIZE;
             procPageAccess = new bool[numPPages]{false};
-            dlog = logger(true);
+            dlog = logger(false);
             data_page = pt_page = 0;
             data_page_faulted = pt_page_faulted = 0;
         }
